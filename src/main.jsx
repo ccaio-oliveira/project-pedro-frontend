243781f58d8ff61d4ae831e29acedcc6a7fc417e
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-07 19:58:09 -0300

    ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.post['Allow-Control-Allow-Origin'] = '*';
 axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
