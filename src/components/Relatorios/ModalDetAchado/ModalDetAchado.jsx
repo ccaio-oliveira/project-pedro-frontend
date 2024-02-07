a13f24afba8003be91537f06e7c6271ab4daa36b
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-07 14:18:12 -0300

    feat: adicionar lógica de visualizar achado

--- src/components/Relatorios/ModalDetAchado/ModalDetAchado.jsx
+++ src/components/Relatorios/ModalDetAchado/ModalDetAchado.jsx
@@ -1,8 +1,23 @@
 import { PropTypes } from 'prop-types';
 import ModalTemplate from '../../Modal/Modal';
+import axios from 'axios';
+import { useAuth } from '../../../context/AuthContext';
+import { useEffect } from 'react';
 
 const ModalDetAchado = ({ achado, onClose }) => {
-    console.log(achado)
+    const { sessao, headers } = useAuth();
+    
+    const handleVisualizacao = async () => {
+        await axios.get(`/api/relatorios/${achado.id}/${sessao.id}`, { headers })
+        .then(response => {
+            console.log(response.data);
+        })
+    }
+
+    useEffect(() => {
+        handleVisualizacao();
+    }, []);
+
     return (
         <>
             <ModalTemplate title="Chamado detalhado" onRequestClose={onClose}>
