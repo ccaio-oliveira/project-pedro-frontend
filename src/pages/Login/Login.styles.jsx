d81ed174f9f447d56d8993c1692a2d0d25fab8d4
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-13 07:21:25 -0300

    style: ajustar layout e adicionar responsividade

--- src/pages/Login/Login.styles.jsx
+++ src/pages/Login/Login.styles.jsx
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 
 export const LoginElement = styled.div`
     display: flex;
+    flex-wrap: wrap;
+
+    @media (max-width: 686px){
+        width: 100%;
+        flex-direction: column;
+
+        margin-top: 30px;
+    }
 `;
 
 export const LeftElement = styled.div`
@@ -13,26 +21,67 @@ export const LeftElement = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+
+    @media (max-width: 686px){
+        width: 100%;
+    }
 `;
 
 export const ImgElement = styled.img`
     width: 450px;
     background: #FFF;
+
+    @media (max-width: 910px){
+        width: 350px;
+    }
+
+    @media (max-width: 686px){
+        width: 250px;
+    }
 `;
 
 export const TextElement = styled.div`
     width: 23rem;
     margin-top: 20px;
+
+    @media (max-width: 910px){
+        width: 20rem;
+    }
+
+    @media (max-width: 686px){
+        width: 50%;
+        text-align: center;
+    }
+
+    @media (max-width: 440px){
+        width: 90%;
+    }
 `;
 
 export const TextH1 = styled.h1`
     font-weight: 400;
     font-size: 34px;
+
+    @media (max-width: 910px){
+        font-size: 28px;
+    }
+
+    @media (max-width: 686px){
+        font-size: 24px;
+    }
+
+    @media (max-width: 440px){
+        font-size: 20px;
+    }
 `;
 
 export const TextP = styled.p`
     margin-top: 10px;
     font-weight: 500;
+
+    @media (max-width: 910px){
+        font-size: 0.9rem;
+    }
 `;
 
 export const RightElement = styled.div`
@@ -46,6 +95,14 @@ export const RightElement = styled.div`
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
+    @media (max-width: 686px){
+        width: 100%;
+        height: 100%;
+        margin-top: 10px;
+
+        border-radius: 17px 17px 0px 0px;
+    }
 `;
 
 export const Direitos = styled.div`
@@ -54,6 +111,11 @@ export const Direitos = styled.div`
 
     text-align: center;
     color: #D9D5D5;
+
+    @media (max-width: 686px){
+        top: 0px;
+        margin: 30px 0px;
+    }
 `;
 
 export const FormElement = styled.form`
@@ -61,6 +123,14 @@ export const FormElement = styled.form`
     flex-direction: column;
 
     margin-top: 20px;
+
+    @media (max-width: 686px){
+        margin-top: 16px;
+    }
+
+    @media (max-width: 440px){
+        width: 90%;
+    }
 `;
 
 export const FormGroupLogin = styled.div`
@@ -68,10 +138,22 @@ export const FormGroupLogin = styled.div`
     flex-direction: column;
 
     margin-top: 20px;
+
+    @media (max-width: 686px){
+        margin-top: 10px;
+    }
 `;
 
 export const FormLabelLogin = styled.label`
     font-size: 1.2rem;
+
+    @media (max-width: 910px){
+        font-size: 1.0rem;
+    }
+
+    @media (max-width: 440px){
+        font-size: 0.9rem;
+    }
 `;
 
 export const InputFormLogin = styled.input`
@@ -90,11 +172,23 @@ export const InputFormLogin = styled.input`
         color: #FFF;
         font-size: 0.9rem;
     }
+
+    @media (max-width: 910px){
+        width: 18rem;
+    }
+
+    @media (max-width: 440px){
+        width: 90%;
+    }
 `;
 
 export const ErrorP = styled.p`
     color: red;
     margin-top: 20px;
+
+    @media (max-width: 910px){
+        font-size: 0.9rem;
+    }
 `;
 
 export const ForgotPasswordP = styled.p`
@@ -106,6 +200,14 @@ export const ForgotPasswordP = styled.p`
     margin: 14px 0px;
 
     cursor: pointer;
+
+    @media (max-width: 910px){
+        font-size: 0.9rem;
+    }
+
+    @media (max-width: 686px){
+        margin: 0px 0px 10px;
+    }
 `;
 
 export const BtnLogin = styled.button`
@@ -120,6 +222,10 @@ export const BtnLogin = styled.button`
     border-radius: 17px;
 
     cursor: pointer;
+
+    @media (max-width: 910px){
+        font-size: 0.9rem;
+    }
 `;
 
 export const BtnNext = styled(BtnLogin)`
@@ -135,6 +241,14 @@ export const IconEye = styled(FontAwesomeIcon)`
     left: 325px;
     bottom: 32px;
     cursor: pointer;
+
+    @media (max-width: 910px){
+        left: 290px;
+    }
+
+    @media (max-width: 440px){
+        left: 350px;
+    }
 `;
 
 export const InfoForgotContainer = styled(ForgotPasswordP)`
