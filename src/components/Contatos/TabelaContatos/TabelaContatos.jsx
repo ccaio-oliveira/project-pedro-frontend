f9b853fba8461d46095f2bde5cba1671254c3864
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-14 07:48:21 -0300

    feat: manipular novas informações de usuários

--- src/components/Contatos/TabelaContatos/TabelaContatos.jsx
+++ src/components/Contatos/TabelaContatos/TabelaContatos.jsx
@@ -3,11 +3,20 @@ import Carregando from '../../Carregando/Carregando';
 import { useAuth } from '../../../context/AuthContext';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { ContainerDataCont, ContainerTextTipo, InfoTabelaContatos, SimbolTipo, TDContato, TextTipo } from './TabelaContatos.styles';
+import { ContainerSearch, IconSearch, InputSearch } from '../../Relatorios/TabelaRelatorios/TabelaRelatorios.styles';
+import { TBody, TH, TR, Tabela } from '../../../global.styles';
+import { THead } from './../../../global.styles';
 
 const TabelaContatos = () => {
     const { sessao, headers, handleSetHeaders } = useAuth(); // Get the sessao object from useAuth context
+    
+    const [searchContato, setSearchContato] = useState(''); // Add searchContato state
+    const [filteredContatos, setFilteredContatos] = useState([]); // Add filteredContatos state
+    
     const [dataContatos, setDataContatos] = useState([]); // Add dataContatos state
-
+    const [errorMessage, setErrorMessage] = useState(''); // Add errorMessage state
+    
     const [isLoading, setIsLoading] = useState(true);
 
     const location = useLocation();
@@ -26,21 +35,106 @@ const TabelaContatos = () => {
             console.log(response.data);
             setDataContatos(response.data);
             setIsLoading(false);
-        })
+        }).catch(error => {
+            console.log(error);
+            setDataContatos([]);
+            setErrorMessage(error.response.data.message);
+            setIsLoading(false);
+        });
     }
+
+    const handleSearchContato = (value) => {
+        setSearchContato(value);
+
+        setFilteredContatos(dataContatos.filter(contato => {
+            if(contato.nome_completo.toLowerCase().includes(value.toLowerCase())){
+                return contato;
+            }
+
+            if(tipoUsuario == 2){
+                if(contato.medico_crm.toLowerCase().includes(value.toLowerCase())){
+                    return contato;
+                }
+
+                if(contato.funcao.toLowerCase().includes(value.toLowerCase())){
+                    return contato;
+                }
+            }
+
+        }));
+    }
+
+    console.log(dataContatos);
 
     useEffect(() => {
         handleSetHeaders(); // Call the handleSetHeaders function
         handleDataContatos(); // Call the handleDataContatos function
+        setIsLoading(true);
     }, [tipoUsuario]);
 
     return (
         <>
             {isLoading ? (
-                <Carregando title="Carregando usuários" /> // Render the Carregando component while loading
+                <Carregando title="Carregando contatos" /> // Render the Carregando component while loading
             ) : (
                 <>
+                    <InfoTabelaContatos>
+                        <ContainerTextTipo>
+                            <SimbolTipo tipo={tipoUsuario} />
+                            <TextTipo tipo={tipoUsuario}>{tipoUsuario == 2 ? 'Médico' : 'Secretária'}</TextTipo>
+                        </ContainerTextTipo>
 
+                        <ContainerDataCont>
+                            <ContainerSearch>
+                                <InputSearch type="text" name="searchContato" value={searchContato} onChange={e => handleSearchContato(e.target.value)} placeholder="Pesquisar contato" />
+                                <IconSearch icon={['fas', 'search']} />
+                            </ContainerSearch>
+                        </ContainerDataCont>
+                    </InfoTabelaContatos>
+
+                    <Tabela>
+                        <THead>
+                            <TR>
+                                <TH>Nome</TH>
+                                <TH>CRM</TH>
+                                <TH>Função</TH>
+                                <TH>Conectar</TH>
+                            </TR>
+                        </THead>
+                        <TBody>
+                            {dataContatos.length !== 0 ? (
+                                searchContato === '' ? (
+                                    dataContatos.map((contato) => (
+                                        <TR key={contato.id}>
+                                            <TDContato>{contato.nome_completo}</TDContato>
+                                            <TDContato>{tipoUsuario == 2 ? contato.medico_crm : 'Outra coisa'}</TDContato>
+                                            <TDContato>{tipoUsuario == 2 ? contato.funcao : 'Secretária'}</TDContato>
+                                            <TDContato>{contato.telefone_whats}</TDContato>
+                                        </TR>
+                                    ))
+                                ) : (
+                                    filteredContatos.length !== 0 ? (
+                                        filteredContatos.map(contato => (
+                                            <TR key={contato.id}>
+                                                <TDContato>{contato.nome_completo}</TDContato>
+                                                <TDContato>{tipoUsuario == 2 ? contato.medico_crm : 'Outra coisa'}</TDContato>
+                                                <TDContato>{tipoUsuario == 2 ? contato.funcao : 'Secretária'}</TDContato>
+                                                <TDContato>{contato.telefone_whats}</TDContato>
+                                            </TR>
+                                        ))
+                                    ) : (
+                                        <TR>
+                                            <TH colSpan="4">Nenhum registro encontrado</TH>
+                                        </TR>
+                                    )
+                                )
+                            ) : (
+                                <TR>
+                                    <TH colSpan="4">{errorMessage}</TH>
+                                </TR>
+                            )}
+                        </TBody>
+                    </Tabela>
                 </>
             )}
         </>
--- src/components/Contatos/TabelaContatos/TabelaContatos.styles.jsx
+++ src/components/Contatos/TabelaContatos/TabelaContatos.styles.jsx
@@ -1,2 +1,26 @@
 import styled from 'styled-components';
+import { ContainerDataRel, ContainerTextGrau, InfoTabelaRelatorio, SimbolGrau, TDChamado, TextGrau } from '../../Relatorios/TabelaRelatorios/TabelaRelatorios.styles';
 
+export const InfoTabelaContatos = styled(InfoTabelaRelatorio)`
+
+`;
+
+export const ContainerTextTipo = styled(ContainerTextGrau)`
+
+`;
+
+export const SimbolTipo = styled(SimbolGrau)`
+    background: ${props => props.tipo == 2 ? '#04293D' : '#164863'};
+`;
+
+export const TextTipo = styled(TextGrau)`
+    color: ${props => props.tipo == 2 ? '#04293D' : '#164863'};
+`;
+
+export const ContainerDataCont = styled(ContainerDataRel)`
+
+`;
+
+export const TDContato = styled(TDChamado)`
+
+`;
