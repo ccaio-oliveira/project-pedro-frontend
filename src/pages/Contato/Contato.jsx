80ceeb1fabfe63f46f5990356a4765327131cf38
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-17 09:28:16 -0300

    fix: corrigir erro de autenticação

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import Contatos from "../../components/Contatos/Contatos";
 import Navbar from "../../components/Navbar/Navbar";
+import { useAuth } from "../../context/AuthContext";
 import { ContainerContato } from "./Contato.styles";
+import Carregando from "../../components/Carregando/Carregando";
 
 const Contato = () => {
+
+    const { handleValidaSessao, sessao, handleSetHeaders } = useAuth();
+
+    const [component, setComponent] = useState(<Carregando title={'Carregando contatos'} />);
+
+    useEffect(() => {
+        handleValidaSessao();
+
+        if(sessao && sessao.loggedin !== false){
+            handleSetHeaders();
+            setTimeout(() => {
+                setComponent(<Contatos />);
+            }, 500);
+        }
+    }, [sessao]);
+
     return(
         <ContainerContato>
             <Navbar item={'contato'} />
-            <Contatos />
+            {component}
         </ContainerContato>
     )
 }
