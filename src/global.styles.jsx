d47e958e0ab48dbd2643b2144640c9ded7ae2d48
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-17 09:35:51 -0300

    styles: ajustar container

--- src/global.styles.jsx
+++ src/global.styles.jsx
@@ -1,9 +1,7 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
-    align-items: center;
-    
+    display: flex;    
     width: 100%;
 `;
 
