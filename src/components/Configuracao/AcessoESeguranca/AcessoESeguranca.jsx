f7dc1fadec3aa3f30c760f60ea9df2163458ac60
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-02-20 00:55:21 -0300

    feat: adicionar modal de email

---
+++ src/components/Configuracao/AcessoESeguranca/AcessoESeguranca.jsx
@@ -0,0 +1,117 @@
+import { ConfigElements, ConfigElementsGroup, ConfigElementsIcon, ConfigElementsP, ConfigElementsSpan, ConfigElementsText } from "../Configuracao.styles";
+import Carregando from "../../Carregando/Carregando";
+import { useAuth } from "../../../context/AuthContext";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ModalEmail from "./ModalEmail/ModalEmail";
+
+library.add(faArrowRightLong);
+
+const AcessoESeguranca = () => {
+    const { sessao, headers } = useAuth();
+    const [dataUser, setDataUser] = useState({});
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalContent, setModalContent] = useState('');
+
+    const handleInfoUser = async () => {
+        setIsLoading(true);
+        await axios.get('/api/usuarioPerfil', {
+            params: {
+                usuario_id: sessao.id
+            },
+            headers
+        }).then((res) => {
+            setDataUser(res.data);
+            setIsLoading(false);
+            console.log(res.data);
+        })
+    }
+
+    const formatNumberPhone = (number) => {
+        return number.replace(/(\d{2})(\d{2})(\d{5})(\d{4})/, "+$1 ($2) $3-$4");
+    }
+
+    const openModal = (content) => {
+        if(content === 'email'){
+            setModalContent(<ModalEmail handleClose={closeModal} email={dataUser.email} />);
+        }
+
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+
+    useEffect(() => {
+        handleInfoUser();
+    }, [modalIsOpen]);
+
+    return (
+        <>
+            {isLoading ? (
+                <Carregando title="Carregando informações do usuário" />
+            ) : (
+                <ConfigElements>
+                    <ConfigElementsGroup onClick={() => openModal('email')}>
+                        <ConfigElementsText>
+                            <ConfigElementsP>Endereço de e-mail registrado</ConfigElementsP>
+                            <ConfigElementsSpan>{dataUser.email}</ConfigElementsSpan>
+                        </ConfigElementsText>
+
+                        <ConfigElementsIcon icon={['fas', 'arrow-right-long']} />
+                    </ConfigElementsGroup>
+
+                    <ConfigElementsGroup>
+                        <ConfigElementsText>
+                            <ConfigElementsP>WhatsApp</ConfigElementsP>
+                            <ConfigElementsSpan>{formatNumberPhone(dataUser.telefone_whats ? dataUser.telefone_whats : '')}</ConfigElementsSpan>
+                        </ConfigElementsText>
+
+                        <ConfigElementsIcon icon={['fas', 'arrow-right-long']} />
+                    </ConfigElementsGroup>
+
+                    <ConfigElementsGroup>
+                        <ConfigElementsText>
+                            <ConfigElementsP>Número de telefone</ConfigElementsP>
+                            <ConfigElementsSpan>{formatNumberPhone(dataUser.telefone_cel ? dataUser.telefone_cel : '')}</ConfigElementsSpan>
+                        </ConfigElementsText>
+
+                        <ConfigElementsIcon icon={['fas', 'arrow-right-long']} />
+                    </ConfigElementsGroup>
+
+                    <ConfigElementsGroup>
+                        <ConfigElementsText>
+                            <ConfigElementsP>Nome disponibilizado</ConfigElementsP>
+                            <ConfigElementsSpan>{dataUser.nome_completo}</ConfigElementsSpan>
+                        </ConfigElementsText>
+
+                        <ConfigElementsIcon icon={['fas', 'arrow-right-long']} />
+                    </ConfigElementsGroup>
+
+                    <ConfigElementsGroup>
+                        <ConfigElementsText>
+                            <ConfigElementsP>Alterar senha</ConfigElementsP>
+                            <ConfigElementsSpan>********</ConfigElementsSpan>
+                        </ConfigElementsText>
+
+                        <ConfigElementsIcon icon={['fas', 'arrow-right-long']} />
+                    </ConfigElementsGroup>
+                </ConfigElements>
+            )}
+
+            {modalIsOpen && (
+                <>
+                    {modalContent}
+                </>
+            )}
+        </>
+    );
+}
+
+export default AcessoESeguranca;
---
+++ src/components/Configuracao/AcessoESeguranca/ModalEmail/ModalEmail.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import ModalTemplate from './../../../Modal/Modal';
+import { InputData } from '../../../../global.styles';
+import axios from 'axios';
+import { useAuth } from '../../../../context/AuthContext';
+import Carregando from '../../../Carregando/Carregando';
+import AlertTemplate from '../../../AlertComponents/AlertTemplate';
+import SuccessAlert from '../../../AlertComponents/SuccessAlert/SuccessAlert';
+import ErrorAlert from '../../../AlertComponents/ErrorAlert/ErrorAlert';
+import { PropTypes } from 'prop-types';
+
+const ModalEmail = ({ handleClose, email }) => {
+    const { headers } = useAuth();
+    const [valueEmail, setValueEmail] = useState(email);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [alertIsOpen, setAlertIsOpen] = useState(false);
+    const [alertTitle, setAlertTitle] = useState('');
+    const [alertMessage, setAlertMessage] = useState('');
+    const [alertType, setAlertType] = useState('');
+
+    const handleEmail = async () => {
+        setIsLoading(true);
+
+        await axios.post('/api/usuarios/changeEmail', {
+            email: email,
+            newEmail: valueEmail
+        }, {
+            headers
+        }).then((res) => {
+            console.log(res.data);
+
+            if(res.data.status === 200){
+                setAlertIsOpen(true);
+                setAlertTitle('Sucesso');
+                setAlertMessage(res.data.message);
+                setAlertType('success');
+
+                setIsLoading(false);
+            } else {
+                setAlertIsOpen(true);
+                setAlertTitle('Erro');
+                setAlertMessage(res.data.message);
+                setAlertType('error');
+
+                setIsLoading(false);
+            }
+        }).catch(() => {
+            setAlertIsOpen(true);
+            setAlertTitle('Erro');
+            setAlertMessage('Erro ao alterar e-mail');
+            setAlertType('error');
+
+            setIsLoading(false);
+        })
+    }
+
+    return (
+        <>
+            <ModalTemplate funcSubmit={handleEmail} onRequestClose={handleClose} title='E-mail registrado' modalFooter submitTitle="Alterar">
+                <InputData type="email" value={valueEmail} onChange={e => setValueEmail(e.target.value)} />
+            </ModalTemplate>
+
+            {isLoading ? (
+                <Carregando title="Alterando e-mail" />
+            ) : (alertIsOpen && (
+                <AlertTemplate title={alertTitle}>
+                    {alertType === 'success' ? (
+                        <SuccessAlert message={alertMessage} close={() => {setAlertIsOpen(false); handleClose();}} />
+                    ) : (
+                        <ErrorAlert message={alertMessage} close={() => setAlertIsOpen(false)} />
+                    )}
+                </AlertTemplate>
+            ))}
+        </>
+    )
+}
+
+ModalEmail.propTypes = {
+    handleClose: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired
+}
+
+export default ModalEmail;
