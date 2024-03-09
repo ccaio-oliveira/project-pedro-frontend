042d41d89ba3df5933728b4c74c44475b2685ec3
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-03-09 15:17:12 -0300

    style: ajustes

--- src/components/Contatos/Contatos.styles.jsx
+++ src/components/Contatos/Contatos.styles.jsx
@@ -5,6 +5,10 @@ import { SecondTextBotaoPrioridade } from './../Relatorios/Relatorios.styles';
 export const ContainerContatos = styled.div`
     width: 95%;
     padding: 20px 20px 0;
+
+    @media (max-width: 776px){
+        padding-left: 5px;
+    }
 `;
 
 export const ContainerBotaoC = styled(ContainerBotaoP)`
