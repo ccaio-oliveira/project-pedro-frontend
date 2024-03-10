da5cb6796cd3bab4a8d0e82d006cb88e062111db
Caio Oliveira <ccaio.oliveira83@gmail.com>
2024-03-10 13:48:29 -0300

    alterando browserouter para hashrouter

--- src/services/routes/Routes.jsx
+++ src/services/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from './../../pages/Login/Login';
 import Dashboard from './../../pages/Dashboard/Dashboard';
 import { AuthProvider } from './../../context/AuthContext';
